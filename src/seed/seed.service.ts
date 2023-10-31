ceaa5ae38e10405658fecb71663b1437af96347b
Andres <andresfelipemurillobonilla@gmail.com>
2023-10-31 13:40:48 -0500

    Seed module

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+
+
+@Injectable()
+export class SeedService {
+
+
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(
+
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+
+  ) { }
+
+  async executeSeed() {
+
+    await this.pokemonModel.deleteMany({});
+
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=20')
+
+    const arrayList = [];
+
+    data.results.forEach(({ name, url }) => {
+
+      const segments = url.split('/');
+      const no = +segments[segments.length - 2]
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      arrayList.push(
+        this.pokemonModel.create({ name, no })
+      );
+    })
+    await Promise.all(arrayList);
+
+    return 'Seed executed';
+  }
+
+
+}
+
+
+
+
